c453cad364cb8b99d3381b6b67dae916c844764d
Muhammad Osama Nusrat <osamanusrat786@gmail.com>
2021-10-18 17:03:30 +0500

    initial commit

---
+++ train_valid_split_yolov5.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+maindir = os.listdir('folder2/')
+try:
+    os.mkdir('coco/')
+except:
+    pass
+try:
+    os.mkdir('coco/images/')
+except:
+    pass
+try:
+    os.mkdir('coco/labels/')
+except:
+    pass
+try:
+    os.mkdir('coco/images/Train/')
+except:
+    pass
+try:
+    os.mkdir('coco/images/Valid/')
+except:
+    pass
+try:
+    os.mkdir('coco/labels/Train/')
+except:
+    pass
+try:
+    os.mkdir('coco/labels/Valid/')
+except:
+    pass
+j = 0
+for i in maindir:
+    OriginalName = os.path.splitext(i)[0]
+    print(OriginalName)
+    extensionsss = os.path.splitext(i)[1]
+    print(extensionsss)
+    if(extensionsss == '.jpg' or extensionsss == '.png' or extensionsss == '.PNG' or extensionsss == '.jpeg' ):
+        print(OriginalName+extensionsss)
+        try:
+            if(j%5):
+                shutil.copy2(f'folder2/{OriginalName+extensionsss}',f'coco/images/Train/{OriginalName+extensionsss}')
+                shutil.copy2(f'folder2/{OriginalName}.txt',f'coco/labels/Train/{OriginalName}.txt')
+            else:
+                shutil.copy2(f'folder2/{OriginalName+extensionsss}',f'coco/images/Valid/{OriginalName+extensionsss}')
+                shutil.copy2(f'folder2/{OriginalName}.txt',f'coco/labels/Valid/{OriginalName}.txt')
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        pass
+
+    j+=1

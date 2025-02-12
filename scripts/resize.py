74c130bebe24cb0aa6113708aa8a98ebe7ac3fdb
Muhammad Osama Nusrat <45511078+mnusrat786@users.noreply.github.com>
2025-02-13 01:46:38 +0500

    Add files via upload

---
+++ scripts/resize.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+import os
+import cv2
+
+arr = os.listdir("folder11/")
+#for loop
+#aaaa
+for aa in arr:
+    aaa = aa.split('.')[0]
+    img = cv2.imread(f"folder11/{aa}")
+    ress = cv2.resize(img,(640,640))
+    cv2.imwrite(f"folder3/{aaa}.png",ress) 
